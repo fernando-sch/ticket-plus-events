dd4ab8f159f9ce6b4a31db3457a1179593056220
Fernando Schneider <93660206+fernando-sch@users.noreply.github.com>
2024-06-20 12:27:58 -0300

    Add ticket validations (#7)

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,6 +1,12 @@
 package domain
 
+import "errors"
+
 type TicketType string
+
+var (
+	ErrTicketPriceInvalid = errors.New("ticket price must be greater than zero")
+)
 
 const (
 	TicketTypeHalf TicketType = "half"
@@ -13,4 +19,22 @@ type Ticket struct {
 	Spot *Spot
 	TicketType TicketType
 	Price float64
+}
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrTicketPriceInvalid
+	}
+
+	return nil
 }
