ab05bee7b54e1c3fe67fa4cbd5e41fb91e8c8432
Fernando Schneider <93660206+fernando-sch@users.noreply.github.com>
2024-06-20 16:29:24 -0300

    Add partner one service (#8)

---
+++ internal/events/infra/service/partner.go
@@ -0,0 +1,17 @@
+package service
+
+type ReservationRequest struct {
+	Spots []string `json:"spots"`
+	EventID string `json:"event_id"`
+	TicketType string `json:"ticket_type"`
+	Email string `json:"email"`
+}
+
+type ReservationResponse struct {
+	ID string `json:"id"`
+	Email string `json:"email"`
+	Spot string `json:"spot"`
+	TicketType string `json:"ticket_type"`
+	Status string `json:"status"`
+	EventID string `json:"event_id"`
+}
